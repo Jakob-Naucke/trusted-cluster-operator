trusted-execution-clusters/operator

test: add LUKS and Attestation Key sync tests
test_luks_key_sync - verify the initial LUKS key upload, re-sync after trustee restart,
and LUKS key deletion on machine removal.

test_attestation_key_sync - verify that attestation keys are registered with the KBS and re-registered after trustee restarts.

Signed-off-by: Roy Kaufman <rkaufman@redhat.com>

test_utils/src/lib.rs
@@ -592,6 +592,49 @@ impl TestContext {
         Ok(())
     }
 
+    pub async fn wait_for_deployment_ready(
+        &self,
+        deployments_api: &Api<Deployment>,
+        deployment_name: &str,
+        timeout_secs: u64,
+    ) -> Result<()> {
+        test_info!(
+            &self.test_name,
+            "Waiting for deployment {} to be ready",
+            deployment_name
+        );
+        let poller = Poller::new()
+            .with_timeout(Duration::from_secs(timeout_secs))
+            .with_interval(Duration::from_secs(5))
+            .with_error_message(format!(
+                "{deployment_name} deployment does not have 1 available replica after {timeout_secs} seconds"
+            ));
+
+        let test_name_owned = self.test_name.clone();
+        poller
+            .poll_async(move || {
+                let api = deployments_api.clone();
+                let name = deployment_name.to_string();
+                let tn = test_name_owned.clone();
+                async move {
+                    let deployment = api.get(&name).await?;
+
+                    if let Some(status) = &deployment.status
+                        && let Some(available_replicas) = status.available_replicas
+                        && available_replicas == 1
+                    {
+                        test_info!(&tn, "{} deployment has 1 available replica", name);
+                        return Ok(());
+                    }
+
+                    Err(anyhow!(
+                        "{name} deployment does not have 1 available replica yet"
+                    ))
+                }
+            })
+            .await
+    }
+
     async fn create_certificate(
         &self,
         service_name: &str,

Jakob-Naucke/trusted-cluster-operator

test_utils/src/lib.rs
@@ -7,7 +7,7 @@ use anyhow::{Context, Result, anyhow};
 use constants::APPROVED_IMAGE_NAME;
 use fs_extra::dir;
 use glob::glob;
-use k8s_openapi::api::apps::v1::{Deployment, DeploymentCondition, DeploymentStatus};
+use k8s_openapi::api::apps::v1::Deployment;
 use k8s_openapi::api::core::v1::{
     ConfigMap, LoadBalancerStatus, Namespace, Secret, Service, ServicePort, ServiceSpec,
     ServiceStatus,
@@ -637,6 +637,35 @@ impl TestContext {
         Ok(())
     }
 
+    pub async fn wait_for_deployment_ready(
+        &self,
+        deployments_api: &Api<Deployment>,
+        deployment_name: &str,
+        timeout_secs: u64,
+    ) -> Result<()> {
+        test_info!(
+            &self.test_name,
+            "Waiting for deployment {} to be ready",
+            deployment_name
+        );
+        let has_available_replica = |d: Option<&Deployment>| {
+            d.and_then(|d| d.status.as_ref())
+                .and_then(|s| s.available_replicas)
+                .is_some_and(|r| r >= 1)
+        };
+        let done = await_condition(
+            deployments_api.clone(),
+            deployment_name,
+            has_available_replica,
+        );
+        timeout(Duration::from_secs(timeout_secs), done)
+            .await
+            .context(format!(
+            "{deployment_name} deployment does not have 1 available replica after {timeout_secs} seconds"
+        ))??;
+        Ok(())
+    }
+
     async fn create_certificate(
         &self,
         service_name: &str,
@@ -982,14 +1011,6 @@ impl TestContext {
             );
         }
 
-        let depl_ready = |depl: Option<&Deployment>| {
-            let chk_cond = |c: &DeploymentCondition| c.type_ == "Available" && c.status == "True";
-            let chk_status =
-                |st: &DeploymentStatus| st.conditions.as_ref().map(|cs| cs.iter().any(chk_cond));
-            let chk = |depl: &Deployment| depl.status.as_ref().and_then(chk_status);
-            depl.and_then(chk).unwrap_or(false)
-        };
-
         let depls: Api<Deployment> = Api::namespaced(self.client.clone(), ns);
         for depl in [
             "trusted-cluster-operator",
@@ -997,10 +1018,8 @@ impl TestContext {
             TRUSTEE_DEPLOYMENT,
             ATTESTATION_KEY_REGISTER_DEPLOYMENT,
         ] {
-            self.info(format!("Waiting for deployment {depl} to be ready"));
-            let done = await_condition(depls.clone(), depl, depl_ready);
-            let ctx = format!("waiting for deployment {depl} to be ready");
-            timeout(scaled_duration(300), done).await.context(ctx)??;
+            self.wait_for_deployment_ready(&depls, depl, scaled_timeout(300))
+                .await?;
         }
 
         let svc = ATTESTATION_KEY_REGISTER_SERVICE;